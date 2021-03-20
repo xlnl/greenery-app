7467615bf5afc83b5c0d8cba9edfafe9a2c95d15
xlnl <lamanhnle@gmail.com>
2021-03-19 20:30:08 -0400

    working through the models and getting the user auth prepared with validations

---
+++ models/plant.js
@@ -0,0 +1,30 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class plant extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      models.plant.belongsTo(models.user)
+      models.plant.belongsTo(models.zone)
+      models.plant.hasMany(models.comment)
+      models.plant.hasMany(models.note)
+    }
+  };
+  plant.init({
+    name: DataTypes.STRING,
+    scientific_name: DataTypes.STRING,
+    zone_id: DataTypes.STRING,
+    image: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'plant',
+  });
+  return plant;
+};
