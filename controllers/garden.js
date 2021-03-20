0523e06a60c329af8c194c586e90f51477100ce2
xlnl <lamanhnle@gmail.com>
2021-03-19 22:10:23 -0400

    stubbed route for my garden page

---
+++ controllers/garden.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const router = express.Router()
+const db = require("../models")
+const isLoggedIn = require("../middleware/isLoggedIn")
+
+
+// GET /garden - shows all user's saved plants from
+// Trefle's global plant database with options to delete a plant
+// from user's garden and to read more about a plant
+router.get("/index", isLoggedIn, (req, res) => {
+    res.send("welcome to my garden!")
+})
+
+// GET /garden/:id - lists more info about a plant + any notes
+// show form to create a new note and a option to delete the plant 
+// from user's garden if user is logged in
+router.get("/:id", isLoggedIn, (req, res) => {
+    res.send("more details about my saved plant")
+})
+
+// POST /garden/:id/notes - posts notes if user is logged in
+router.post("/:id/notes", isLoggedIn, (req, res) => {
+    res.send("note data to db")
+})
+
+module.exports = router
