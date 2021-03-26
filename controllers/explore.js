cbe9166bf112aaaa2def2c3da79f4c690e87aa26
xlnl <lamanhnle@gmail.com>
2021-03-25 21:21:54 -0400

    form successfully queried for plants

--- controllers/explore.js
+++ controllers/explore.js
@@ -5,15 +5,28 @@ const isLoggedIn = require("../middleware/isLoggedIn")
 const axios = require("axios")
 
 
-// GET /explore - shows a form to users to search through
-// Trefle's global plant database; renders query results 
+// GET /explore/index - shows a form to users to search through
 router.get("/index", (req, res) => {
-    const plantUrl = `https://trefle.io/api/v1/plants?&token=${process.env.ACCESS_TOKEN}`
-    axios.get(plantUrl)
+    res.render("explore/index")
+})
+
+// GET /explore/plants
+// Trefle's global plant database; renders query results from db
+router.get("/plants", (req, res) => {
+    let plant = req.query.q
+    var qs = {
+        params: {
+            token: `${process.env.ACCESS_TOKEN}`,
+            q: plant
+        }
+    }
+    const plantUrl = "https://trefle.io/api/v1/plants/search"
+
+    axios.get(plantUrl, qs)
     .then(response => {
-        const plant = response.data.data
-        // console.log(plant)
-        res.render('explore/index', { plant: plant })
+        const results = response.data.data
+        console.log(results)
+        res.render('explore/plants', { plant: results })
     })
     .catch(err => {
         console.log("errrrr!!!:", err)
